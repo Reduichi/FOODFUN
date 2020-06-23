c49585d163472276b1e96d691a895585028c7028
Reduichi <57396815+Reduichi@users.noreply.github.com>
2020-06-23 21:30:41 +0800

    0623戰鬥條狀

---
+++ FOODFUN/Assets/Script/CountTime.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountTime : MonoBehaviour
+{
+    [Header("時間條")]
+    public Image TimeBar;
+    [Header("時間")]
+    public float time;
+    private float Percent;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        FileBar();
+    }
+    public void FileBar()
+    {
+        if (time == 60) return;
+        time += Time.deltaTime;
+        TimeBar.fillAmount = time /60;
+    }
+}
