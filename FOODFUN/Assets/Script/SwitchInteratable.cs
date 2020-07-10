17f24f880db5b84a135ebcebbb3fb5116a08cbaa
Reduichi <57396815+Reduichi@users.noreply.github.com>
2020-07-10 18:52:44 +0800

    20200710

---
+++ FOODFUN/Assets/Script/SwitchInteratable.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchInteratable : MonoBehaviour
+{
+    public GameObject chooce,Player,Pet,Castle;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void ClosePlayer()
+    {
+        chooce.SetActive(true);
+        Player.SetActive(false);
+    }
+    public void OpenPlayer()
+    {
+        Player.SetActive(true);
+        chooce.SetActive(false);
+    }
+    public void ClosePet()
+    {
+        chooce.SetActive(true);
+        Pet.SetActive(false);
+    }
+    public void OpenPet()
+    {
+        Pet.SetActive(true);
+        chooce.SetActive(false);
+    }
+    public void CloseCastle()
+    {
+        chooce.SetActive(true);
+        Castle.SetActive(false);
+    }
+    public void OpenCastle()
+    {
+        Castle.SetActive(true);
+        chooce.SetActive(false);
+    }
+}
